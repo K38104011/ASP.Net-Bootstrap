c3a9f4abc808e2f76d8170f03fbf1f35177ac41b
K38104011 <phamdanghoanggiang@gmail.com>
2016-09-21 20:39:49 +0700

    add paginated

---
+++ SportsStore.WebUI/Controllers/AnotherProductController.cs
@@ -0,0 +1,52 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AnotherProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public AnotherProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult Index(AnotherProductListViewModel model)
+        {
+            var products = repository.Products;
+
+            if (model.searchString != null)
+            {
+                model.page = 1;
+            }
+            else
+            {
+                model.searchString = model.currentSearchString;
+            }
+
+            model.currentSearchString = model.searchString;
+
+            if (!String.IsNullOrEmpty(model.searchString))
+            {
+                products = products.Where(s => s.Name.Contains(model.searchString) 
+                    || s.Price.ToString().Contains(model.searchString) 
+                    || s.Category.Contains(model.searchString)
+                    || s.Description.Contains(model.searchString)
+                );
+            }
+
+            int pageNumber = model.page ?? 1;
+            model.Products = products.ToPagedList(pageNumber, PageSize);
+            return View(model);
+        }
+
+    }
+}
---
+++ SportsStore.WebUI/Models/AnotherProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.WebUI.Models
+{
+    public class AnotherProductListViewModel
+    {
+        public PagedList.IPagedList<Product> Products { get; set; }
+        public string searchString { get; set; }
+        public string currentSearchString { get; set; }
+        public int? page { get; set; }
+    }
+}
