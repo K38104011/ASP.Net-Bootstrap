d54b41c02a7634c2ae5038a6e4c3bd8d0fd6f4cf
K38104011 <phamdanghoanggiang@gmail.com>
2016-09-22 22:32:03 +0700

    add advanced search

--- SportsStore.WebUI/Controllers/AnotherProductController.cs
+++ SportsStore.WebUI/Controllers/AnotherProductController.cs
@@ -12,7 +12,10 @@ namespace SportsStore.WebUI.Controllers
     public class AnotherProductController : Controller
     {
         private IProductRepository repository;
-        public int PageSize = 4;
+        private int PageSize = 4;
+        private List<string> _searchFilters = new string[] {
+            "All", "Price", "Description", "Name", "Category"
+        }.ToList();
 
         public AnotherProductController(IProductRepository productRepository)
         {
@@ -30,21 +33,34 @@ namespace SportsStore.WebUI.Controllers
             else
             {
                 model.searchString = model.currentSearchString;
+                model.selectedFilter = model.currentSelectedFilter;
             }
 
             model.currentSearchString = model.searchString;
+            model.currentSelectedFilter = model.selectedFilter;
 
             if (!String.IsNullOrEmpty(model.searchString))
             {
-                products = products.Where(s => s.Name.Contains(model.searchString) 
-                    || s.Price.ToString().Contains(model.searchString) 
-                    || s.Category.Contains(model.searchString)
-                    || s.Description.Contains(model.searchString)
-                );
+                switch (model.selectedFilter)
+                {
+                    case "All" : products = products.Where(s => s.Name.Contains(model.searchString)
+                       || s.Price.ToString().Contains(model.searchString)
+                       || s.Category.Contains(model.searchString)
+                       || s.Description.Contains(model.searchString)
+                   ); break;
+                    case "Price" : products = products.Where(s => s.Price.ToString().Contains(model.searchString)); break;
+                    case "Description" : products = products.Where(s =>
+                       s.Description.IndexOf(model.searchString, StringComparison.OrdinalIgnoreCase) >= 0); break;
+                    case "Name": products = products.Where(s =>
+                       s.Name.IndexOf(model.searchString, StringComparison.OrdinalIgnoreCase) >= 0); break;
+                    case "Category": products = products.Where(s =>
+                       s.Category.IndexOf(model.searchString, StringComparison.OrdinalIgnoreCase) >= 0); break;
+                }
             }
 
             int pageNumber = model.page ?? 1;
             model.Products = products.ToPagedList(pageNumber, PageSize);
+            
             return View(model);
         }
 
--- SportsStore.WebUI/Models/AnotherProductListViewModel.cs
+++ SportsStore.WebUI/Models/AnotherProductListViewModel.cs
@@ -9,7 +9,10 @@ namespace SportsStore.WebUI.Models
     public class AnotherProductListViewModel
     {
         public PagedList.IPagedList<Product> Products { get; set; }
+        public IEnumerable<string> Filters { get; set; }
+        public string selectedFilter { get; set; }
         public string searchString { get; set; }
+        public string currentSelectedFilter { get; set; }
         public string currentSearchString { get; set; }
         public int? page { get; set; }
     }
